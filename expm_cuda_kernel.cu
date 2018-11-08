ad7950240395bd737fb100d83e00245a05afbdb7
Nicki Skafte <skaftenicki@gmail.com>
2018-11-08 14:43:46 +0100

    working on the cuda part

--- expm_cuda_kernel.cu
+++ expm_cuda_kernel.cu
@@ -3,22 +3,52 @@
 #include <cuda_runtime.h>
 #include <vector>
 
-// Kernel declaration
-__global__ void cuda_kernel_forward(float* __restrict__ output, 
-                                    const float* __restrict__ input){
-    //const int i = blockIdx.x*blockDim.x + threadIdx.x;
-    return;
+
+__global__ void matrixMultiplicationKernel(float* A, float* B, float* C, int N) {
+
+    int ROW = blockIdx.y*blockDim.y+threadIdx.y;
+    int COL = blockIdx.x*blockDim.x+threadIdx.x;
+
+    float tmpSum = 0;
+
+    if (ROW < N && COL < N) {
+        // each thread computes one element of the block sub-matrix
+        for (int i = 0; i < N; i++) {
+            tmpSum += A[ROW * N + i] * B[i * N + COL];
+        }
+    }
+    C[ROW * N + COL] = tmpSum;
 }
+
+__global__ void pade13(const double* A, double* expmA, const int N, const int row, const int matsize){
+    int b = blockIdx.x*blockDim.x+threadIdx.x;
+    int i = blockIdx.z*blockDim.z+threadIdx.z;
+    int j = blockIdx.y*blockDim.y+threadIdx.y;
+    
+    if(b < N && i < row && j < row){
+    
+    }
+}
+
 
 // Kernel launcher declaration
 at::Tensor expm_cuda_forward(at::Tensor input){
-    auto output = at::zeros_like(input);    
-    /*
-    const int blockSize = 256;
-    const int numBlocks = ceil((N + blockSize - 1) / blockSize);
-    square_kernel_forward<<<numBlocks, blockSize>>>(output.data<float>(),
-                                                    input.data<float>(),
-                                                    N);
-    */                                                    
+    // Problem size
+    const auto N = input.size(0);
+    const auto row = input.size(1);
+    const auto col = input.size(2);
+    const auto matsize = row*col;
+    
+    // Allocate output
+    auto output = at::zeros_like(input);
+    auto A = input.data<double>();
+    auto expmA = output.data<double>();
+    
+    // Strategy
+    // - compute n_squaring on cpu
+    // - find pade matrices with own kernel
+    // - use cusolverDnDgetrs to solve the matrix system (probably multiple gpu streams)
+    // - use unsquaring, probably own kernel for efficiency
+                                                       
     return output;
 }
